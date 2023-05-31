652eba27ca9266118dd6dae22d7d8fa0b456c5f6
ssstumbles <sara.rosic@gmail.com>
2023-05-31 18:09:24 -0400

    got over a few big hurdles in terms of seeding stuff. More debugging to go but it seems less abstract

--- seed/flights.js
+++ seed/flights.js
@@ -1,3 +1,4 @@
+//oh my god I got stuck seeding this because flights vs Flights. the lowercase f uppercase FUCKED me
 const db = require('../db')
 const Chance = require('chance')
 const { Airport, Flight } = require('../models')
@@ -6,9 +7,10 @@ const chance = Chance()
 //sidenote, chance has a section on dice which will be useful for games
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
+
 const createFlights = async() => {
-    const flights = [Array(15)].map((flight) => {
-        return new flight({
+    const flights = Array.from({length:15}, async () => {
+        return new Flight({
             airline: chance.sentence({words: 2}),
             flight_number: chance.integer({min:111, max:999}),
             //hmm I wonder if 0 are placeholders? if min was 000 would flight number be '000' or '0'
@@ -16,16 +18,18 @@ const createFlights = async() => {
             //can math functions be used to round to a dollar-looking int
             numberOfSeats: chance.integer({min:80, max:250}),
             departure: chance.date(),
-            departingAirport: airports.map((airport) => flight.id),
-            arrivingAirport: airports.map((airport) => flight.id)
+            departingAirport: Airport._id,
+            arrivingAirport: Airport._id
         })
     })
     await Flight.insertMany(flights)
     console.log('flights createed')
     return airports
+    //let airports = await Airport.find({})
 }
 
 const run = async () => {
+    const flights = await createFlights
     await createFlights(flights)
     db.close()
 }
