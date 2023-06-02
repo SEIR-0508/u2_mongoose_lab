a563b1fbb275be6bd8dcf900ca5f4601371772cb
Dan Mulka <danielmulka92@gmail.com>
2023-06-02 07:55:30 -0500

    mongoose_hw

--- query.js
+++ query.js
@@ -35,12 +35,51 @@ const createFlight = async () => {
     console.log(flight)
 }
 
+const updateFlight = async () => {
+    let updateAir = await Flights.updateOne({
+        airline: 'United Airlines',
+        flight_number: 8,
+        price: 250,
+        number_of_seats: 2,
+        departing_airport: "64792ba40942641e39b8d736",
+        arrival_airpot: [],
+        departure_date_time: '6/1/2023 - 01:30'
+    })
+    console.log(updateAir)
+}
+
+const updateAirport = async () => {
+    let updatePort = await Airports.updateOne({
+      name:'Los Angeles International Airport',  
+      location: '1 World Way, Los Angeles, CA 90045',
+      code: 'LAX'
+})
+coonsole.log(updatePort)
+}
+
+const deleteFlight = async () => {
+    let removeFlight = await Flights.deleteOne({flight_number: 9})
+    console.log(removeFlight)
+}
+
+const deleteAirport = async () => {
+    let removeAir = await Flights.deleteOne({})
+    console.log(removeAir)
+}
+
+
+
+
 
 
 async function main() {
+     await deleteAirport()
+     //await deleteFlight()
+    //await updateAirport
+   // await updateFlight()
    // await createFlight()
     // await flightId()
-    await allFlights()
+    //await allFlights()
     // await allAirports()
     try {
     } catch (error) {
