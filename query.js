e0881cc49ac03e3b25f8e43e76b9441ed933053a
Elliot <ellthrowaway2@icloud.com>
2023-06-04 18:47:58 -0400

    more updates; still can't quite get data to seed properly

--- query.js
+++ query.js
@@ -1,35 +1,24 @@
 const db = require('./db')
 const { Airport, Flight } = require('./models')
 
-const findAllAirports = async () => {
-    const airports = await Airport.find()
-    console.log('airports', airports)
-}
-
+// find all flights
 const findAllFlights = async () => {
     const flights = await Flight.find()
     console.log('flights', flights)
 }
 
-const run = async () => {
-    try {
-        await findAllAirports()
-        await findAllFlights()
-    } catch (error) {
-        console.log(error)
-    } finally {
-        await db.close()
-    }
+// find all airports
+const findAllAirports = async () => {
+    const airports = await Airport.find()
+    console.log('airports', airports)
 }
 
-run()
-
-
-
-// USER STORIES:
-
 // As A User (AAU), I want to view a list of all flights and airports (index functionality) that displays each flight's airline, airport, flight no., and departure date/time (consider formatting the departs property).
-
+const findFlightsAndAirports = async () => {
+    const flight = await Flight.find()
+    const airport = await Airport.find()
+    console.log(`Flight: ${flight}, Airport: ${airport}`)
+}
 
 
 // AAU, I want to be able to access the details for each of these objects via a Show route based on the object's ID
@@ -39,3 +28,17 @@ run()
 // AAU, I want to be able to update the details for my Flights and Airports
 
 // AAU, I want to be able to delete any Flight and Airport
+
+const run = async () => {
+    try {
+        await findAllAirports()
+        await findAllFlights()
+        await findFlightsAndAirports()
+    } catch (error) {
+        console.log(error)
+    } finally {
+        await db.close()
+    }
+}
+
+run()
